dec87394e6d6d0b5164da90ceadb55374775dbec
MichelRLima <michelrocha502@gmail.com>
2023-06-19 01:43:23 -0300

    adicionado express session, portal de administrador e adicionar metodo para adicionar postagem junto com upload de imagens

--- Posts.js
+++ Posts.js
@@ -1,17 +1,27 @@
-var mongoose = require ('mongoose')
+// Importando o módulo 'mongoose'
+var mongoose = require('mongoose');
 
-var Schema = mongoose.Schema
+// Criando uma instância do esquema do MongoDB
+var Schema = mongoose.Schema;
 
-var postSchema = new Schema ({
-    titulo:String,
-    image:String,
-    categoria:String,
+// Definindo o esquema para os posts
+var postSchema = new Schema(
+  {
+    titulo: String,
+    image: String,
+    categoria: String,
     conteudo: String,
     slug: String,
     autor: String,
-    views: Number
-}, {collection:'posts'})
+    views: Number,
+  },
+  { collection: 'posts' } // Nome da coleção no banco de dados
+);
 
-var Posts = mongoose.model("Posts", postSchema)
+// Criando o modelo para os posts usando o esquema definido acima
+var Posts = mongoose.model('Posts', postSchema);
 
+// Exportando o modelo para uso em outros arquivos
 module.exports = Posts;
+
+// Fim do código
--- index.js
+++ index.js
@@ -1,121 +1,218 @@
-const express = require ('express') // Importando o módulo 'express'
+// Importando os módulos necessários
+const express = require('express');
+const fileupload = require('express-fileupload');
+const path = require('path');
+const app = express();
+const session = require('express-session');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const Posts = require('./Posts.js');
 
-const path = require('path') // Importando o módulo 'path' para manipulação de caminhos de arquivo e diretório
-const app = express() // Criando uma instância do aplicativo Express
+// Conectando ao banco de dados MongoDB
+mongoose
+  .connect(
+    'mongodb+srv://michelrocha502:root@cluster0.ozgxq3o.mongodb.net/dankicode?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log('Conectado ao banco de dados com sucesso');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
-const mongoose = require ('mongoose') // Importando o módulo 'mongoose' para interagir com o MongoDB
-var bodyParser = require('body-parser') // Importando o módulo 'body-parser' para analisar corpos de solicitações HTTP
+// Configurações e middlewares
+app.use(
+  session({
+    secret: 'teste',
+    cookie: { maxAge: 3600000 },
+  })
+);
 
-const Posts = require('./Posts.js') // Requerendo o arquivo 'Posts.js' que contém o modelo dos dados
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, 'temp'),
+  })
+);
 
-mongoose.connect('mongodb+srv://michelrocha502:root@cluster0.ozgxq3o.mongodb.net/dankicode?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true })
-// Estabelecendo a conexão com o banco de dados MongoDB
-
-.then(() => { // Caso a conexão seja estabelecida com sucesso
-    console.log("conectado com sucesso") // Exibindo uma mensagem de sucesso no console
-})
-.catch((err) => { // Caso ocorra algum erro durante a conexão
-    console.log(err.message) // Exibindo a mensagem de erro no console
-})
-
-
-// Configuração do mecanismo de renderização do EJS para arquivos HTML
 app.engine('ejs', require('ejs').renderFile);
-// Definição do mecanismo de visualização como HTML
 app.set('view engine', 'ejs');
-// Configuração do middleware para servir arquivos estáticos da pasta 'public'
 app.use('/public', express.static(path.join(__dirname, 'public')));
-// Definição do diretório de visualizações como '/views'
 app.set('views', path.join(__dirname, '/pages'));
-// Configuração do middleware 'body-parser' para analisar corpos de solicitações JSON
 app.use(bodyParser.json());
-// Configuração do middleware 'body-parser' para analisar corpos de solicitações codificadas na URL
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Rota para a página inicial
 app.get('/', (req, res) => {
   if (req.query.buscar == null) {
-      // Consulta todos os posts ordenados pelo campo '_id' em ordem decrescente
-      Posts.find().sort({_id: -1}).exec()
-      .then(posts => {
-          // Mapeia os dados dos posts para um novo formato
-          posts = posts.map((val) => {
-              return {
-                  titulo: val.titulo,
-                  conteudo: val.conteudo,
-                  descricaoCurta: val.conteudo.substring(0, 100),
-                  image: val.image,
-                  slug: val.slug,
-                  categoria: val.categoria,
-                  views: val.views
-              }
-          })
-          res.render('home', {posts: posts}) // Renderiza o template 'home' passando os posts como dados
-      })
-      .catch(err => {
-          console.error(err)
-      })
-  } else {
-      // Consulta os posts que possuem o título correspondendo à busca do usuário
-      Posts.find({titulo: {$regex: req.query.buscar, $options: 'i'}})
+    // Consulta todos os posts ordenados pelo campo '_id' em ordem decrescente
+    Posts.find()
+      .sort({ _id: -1 })
+      .exec()
       .then((posts) => {
-          if (posts && posts.length > 0) {
-              res.render('busca', {posts: posts}) // Renderiza o template 'busca' passando os posts encontrados como dados
-          } else {
-              res.render('busca', {posts: []}) // Renderiza o template 'busca' com um array vazio caso não haja posts encontrados
-          }
+        // Mapeia os dados dos posts para um novo formato
+        posts = posts.map((val) => {
+          return {
+            titulo: val.titulo,
+            conteudo: val.conteudo,
+            descricaoCurta: val.conteudo.substring(0, 100),
+            image: val.image,
+            slug: val.slug,
+            categoria: val.categoria,
+            views: val.views,
+          };
+        });
+        res.render('home', { posts: posts }); // Renderiza o template 'home' passando os posts como dados
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  } else {
+    // Consulta os posts que possuem o título correspondendo à busca do usuário
+    Posts.find({ titulo: { $regex: req.query.buscar, $options: 'i' } })
+      .then((posts) => {
+        if (posts && posts.length > 0) {
+          res.render('busca', { posts: posts }); // Renderiza o template 'busca' passando os posts encontrados como dados
+        } else {
+          res.render('busca', { posts: [] }); // Renderiza o template 'busca' com um array vazio caso não haja posts encontrados
+        }
       })
       .catch((error) => {
-          console.error(error)
-          res.status(500).send('Erro ao buscar os posts')
-      })
+        console.error(error);
+        res.status(500).send('Erro ao buscar os posts');
+      });
   }
-})
+});
 
 // Rota para exibir um post individual com base no slug
-app.get("/:slug", (req, res) => {
+app.get('/:slug', (req, res) => {
   // Utiliza o método findOneAndUpdate() do Mongoose para atualizar o campo 'views' do documento encontrado
   Posts.findOneAndUpdate(
-      {slug: req.params.slug}, // Busca o post com base no slug fornecido
-      {$inc: {views: 1}}, // Incrementa o valor do campo 'views' em 1
-      {new: true} // Retorna o documento atualizado
+    { slug: req.params.slug }, // Busca o post com base no slug fornecido
+    { $inc: { views: 1 } }, // Incrementa o valor do campo 'views' em 1
+    { new: true } // Retorna o documento atualizado
   )
-  .then((resposta) => {
+    .then((resposta) => {
       if (resposta) {
-          // Consulta todos os posts ordenados pelo campo '_id' em ordem decrescente
-          Posts.find().sort({_id: -1}).exec()
+        // Consulta todos os posts ordenados pelo campo '_id' em ordem decrescente
+        Posts.find()
+          .sort({ _id: -1 })
+          .exec()
           .then((posts) => {
-              posts = posts.map((val) => {
-                  return {
-                      titulo: val.titulo,
-                      conteudo: val.conteudo,
-                      descricaoCurta: val.conteudo.substring(0, 100),
-                      image: val.image,
-                      slug: val.slug,
-                      categoria: val.categoria,
-                      views: val.views
-                  }
-              })
-              res.render('single', {noticia: resposta, posts: posts}) // Renderiza o template 'single' passando a notícia e a lista de posts como dados
+            posts = posts.map((val) => {
+              return {
+                titulo: val.titulo,
+                conteudo: val.conteudo,
+                descricaoCurta: val.conteudo.substring(0, 100),
+                image: val.image,
+                slug: val.slug,
+                categoria: val.categoria,
+                views: val.views,
+              };
+            });
+            res.render('single', { noticia: resposta, posts: posts }); // Renderiza o template 'single' passando a notícia e a lista de posts como dados
           })
           .catch((err) => {
-              console.error(err)
-          })
+            console.error(err);
+          });
       } else {
-          // Lida com o caso em que nenhum documento foi encontrado com o slug fornecido
-          // Por exemplo, redirecionando o usuário para uma página de erro ou exibindo uma mensagem adequada.
+        // Lida com o caso em que nenhum documento foi encontrado com o slug fornecido
+        // Por exemplo, redirecionando o usuário para uma página de erro ou exibindo uma mensagem adequada.
       }
-  })
-  .catch((err) => {
-      console.error(err)
-  })
-})
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+// Rota para o login do administrador
+app.post('/admin/login', (req, res) => {
+  usuario.map((val) => {
+    if (val.login === req.body.login && val.senha === req.body.senha) {
+      req.session.login = 'Michel';
+    }
+  });
+  res.redirect('/admin/login');
+});
+
+// Rota para o cadastro de um novo post
+app.post('/admin/cadastro', (req, res) => {
+  let formato = req.files.arquivo.name.split('.');
+  var imagem = '';
+  if (formato[formato.length - 1] == 'jpg') {
+    imagem = new Date().getTime() + '.jpg';
+    req.files.arquivo.mv(__dirname + '/public/images/' + imagem);
+  } else {
+    fs.unlinkSync(req.files.arquivo.tempFilePath);
+  }
+
+
+  Posts.create({
+    titulo: req.body.titulo_noticia,
+    image: 'http://localhost:3000/public/images/' + imagem,
+    categoria: req.body.categoria,
+    conteudo: req.body.noticia,
+    slug: req.body.slug,
+    autor: req.body.autor,
+    views: 0,
+  });
+
+  res.redirect('/admin/login');
+});
+
+// Rota para exclusão de um post
+app.get('/admin/delete/:id', (req, res) => {
+  Posts.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.redirect('/admin/login');
+    })
+    .catch(() => {
+      // Lida com o erro, se necessário
+    });
+});
+//usuario administrador
+var usuario = [
+    {
+        login:'administrador',
+        senha:'@pwdy2k3'
+    }
+]
+
+
+// Rota para a página de login do administrador
+app.get('/admin/login', (req, res) => {
+  if (req.session.login == null) {
+    res.render('admin-login');
+  } else {
+    Posts.find()
+      .sort({ _id: -1 })
+      .exec()
+      .then((posts) => {
+        posts = posts.map((val) => {
+          return {
+            id: val._id,
+            titulo: val.titulo,
+            conteudo: val.conteudo,
+            descricaoCurta: val.conteudo.substring(0, 100),
+            image: val.image,
+            slug: val.slug,
+            categoria: val.categoria,
+            views: val.views,
+          };
+        });
+        res.render('admin-painel', { posts: posts }); // Renderiza o template 'admin-painel' passando os posts como dados
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+});
 
 const port = process.env.PORT || 3000; // Utiliza a porta fornecida pelo Heroku ou a porta 3000 como padrão
 app.listen(port, () => {
   console.log(`SERVIDOR LIGADO na porta ${port}`);
-})
+});
 
+// Fim do código
